0dd4fbb5e68ea5c0f2753f521db44fdab35e012f
Bamidele Odunlami <bamideleodunlami@Bamideles-MBP.lan>
2023-08-07 23:52:52 +0100

    First commit

--- public/js/verify.js
+++ public/js/verify.js
@@ -1,15 +1,13 @@
-$(document).ready(()=>{
-  window.addEventListener(
-    'message',
-    function(event) {
-      console.log('Message received', event.data);
+window.addEventListener(
+  'message',
+  function(event) {
+    console.log('Message received', event.data);
 
-      if (event.data.eventType === 'SUCCESS') {
-        window.location.href='/profile#kyc-bar'
-      } else if (event.data.eventType === "ERROR") {
-        window.location.href='/profile#kyc-bar'
-       const errorCode = event.data.eventCode;
-      }
+    if (event.data.eventType === 'SUCCESS') {
+      window.location.href='/profile#kyc-bar'
+    } else if (event.data.eventType === "ERROR") {
+      window.location.href='/profile#kyc-bar'
+     const errorCode = event.data.eventCode;
     }
-  );  
-})
+  }
+);  
