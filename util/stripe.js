497d9259da872c73dab0e7f14307b68b1de1d9d5
bamiodunlami18 <bamiodunlami18@gmail.com>
2025-10-08 01:36:17 +0100

    update

--- util/stripe.js
+++ util/stripe.js
@@ -1,2 +1,6 @@
-const stripe = require ('stripe')(process.env.WOSIWOSI_STRIPE_KEY);
-module.exports=stripe
+// util/stripe.js
+const Stripe = require('stripe');
+const stripe = new Stripe(process.env.WOSIWOSI_STRIPE_KEY, {
+  apiVersion: '2024-06-20', // or whichever you want
+});
+module.exports = stripe;
